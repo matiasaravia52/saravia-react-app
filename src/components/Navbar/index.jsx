249001592f85d674076258ccd1721bee2172d8f2
Matias Saravia <matiasaravia52@gmail.com>
2021-09-02 19:50:08 -0300

    Se agrega navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,23 @@
+const { Navbar, Container, Nav, NavDropdown } = require("react-bootstrap");
+
+const NavbarComponent = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">React-App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    )
+}
+
+export default NavbarComponent;
